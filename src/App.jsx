52a394ebce99b7af349bb90622ca83f13fe0a281
Rafal Zagorny <zagorny.rafal@gmail.com>
2026-01-29 15:21:14 +0100

    wing model loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+/* eslint-disable react/no-unknown-property */
 import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+
+function Wing(props) {
+  const { scene } = useGLTF('/wing.glb');
+  return <primitive object={scene} {...props} />;
+}
 
 function App() {
   return (
     <>
       <div id="canvas-container">
-        <Canvas>
-          <ambientLight intensity={0.1} />
-          <directionalLight color="red" position={[0, 0, 5]} />
-          <mesh>
-            <boxGeometry args={[2, 2, 2]} />
-            <meshStandardMaterial />
-          </mesh>
+        <Canvas camera={{ position: [0, 4, 5], fov: 50 }} dpr={[1, 2]}>
+          <ambientLight intensity={0.8} />
+          {/* <spotLight position={[10, 10, 0]} angle={0.15} penumbra={1} decay={0} intensity={1} /> */}
+          <hemisphereLight color="white" position={[0, 0, 0]} intensity={1.5} />
+          <Wing position={[0, 0, 0]} rotation={[0, Math.PI, 0]} />
+          <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 1.75} />
         </Canvas>
       </div>
     </>
