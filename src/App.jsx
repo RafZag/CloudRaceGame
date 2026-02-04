791791d359b05ccc433a5cffd1bd4c9bcad74cd9
Rafal Zagorny <zagorny.rafal@gmail.com>
2026-02-04 14:55:02 +0100

    enviroment lightning

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 /* eslint-disable react/no-unknown-property */
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { OrbitControls, useGLTF, Environment } from '@react-three/drei';
+import { useEffect } from 'react';
+import * as THREE from 'three';
+import envUrl from './assets/golden_gate_hills_2k.exr?url';
 
 function Wing(props) {
   const { nodes, materials } = useGLTF('/wing.glb');
 
   return (
     <group {...props}>
-      <mesh geometry={nodes['wing'].geometry} material={materials.wing} />
-      <mesh geometry={nodes['lines'].geometry} material={materials.lines} />
-      <mesh geometry={nodes['harness'].geometry} material={materials.Harness} />
-      <mesh geometry={nodes['head'].geometry} material={materials.helmet} />
+      <mesh geometry={nodes['wing'].geometry} material={materials.wing} castShadow />
+      <mesh geometry={nodes['lines'].geometry} material={materials.lines} castShadow />
+      <mesh geometry={nodes['harness'].geometry} material={materials.Harness} castShadow />
+      <mesh geometry={nodes['head'].geometry} material={materials.helmet} castShadow />
     </group>
   );
 }
 
 function World(props) {
   const { scene } = useGLTF('/world_slopes.glb');
+
+  useEffect(() => {
+    scene.traverse((child) => {
+      if (child.isMesh) {
+        child.receiveShadow = true;
+      }
+    });
+  }, [scene]);
 
   return <primitive object={scene} {...props} />;
 }
@@ -27,12 +38,36 @@ function App() {
   return (
     <>
       <div id="canvas-container">
-        <Canvas camera={{ position: [0, 12, 8], fov: 60 }} dpr={[1, 2]}>
+        <Canvas camera={{ position: [0, 12, 8], fov: 60 }} dpr={[1, 2]} shadows={{ type: THREE.PCFSoftShadowMap }}>
+          <Environment files={envUrl} environmentRotation={[0, -Math.PI, 0]} environmentIntensity={0.7} />
           {/* <ambientLight intensity={2} /> */}
           {/* <spotLight position={[0, 10, 0]} angle={0.5} penumbra={1} decay={0} intensity={1.2} /> */}
-          <rectAreaLight position={[0, 20, 0]} rotation={[-Math.PI / 2, 0, 0]} intensity={2.5} width={20} height={20} />
-          <hemisphereLight color="white" position={[0, 0, 0]} intensity={1.5} />
-          <Wing position={[0, 6, 0]} rotation={[0, Math.PI, 0]} />
+          {/* <rectAreaLight
+            position={[0, 20, 0]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            intensity={2.5}
+            width={20}
+            height={20}
+            castShadow
+          /> */}
+          <directionalLight
+            position={[0, 12, 0]}
+            intensity={1.0}
+            castShadow
+            shadow-radius={0.5}
+            shadow-bias={-0.0005}
+            shadow-normalBias={0.02}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+            shadow-camera-near={1}
+            shadow-camera-far={50}
+            shadow-camera-left={-15}
+            shadow-camera-right={15}
+            shadow-camera-top={15}
+            shadow-camera-bottom={-15}
+          />
+          {/* <hemisphereLight color="white" position={[0, 0, 0]} intensity={1.5} /> */}
+          <Wing position={[0, 8, 0]} rotation={[0, Math.PI, 0]} />
           <World position={[0, 0, 0]} />
           {/* <mesh rotation={[-Math.PI / 2, 0, 0]}>
             <planeGeometry args={[8, 8]} />
